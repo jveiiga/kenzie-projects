8a92acc9c16b8c40d38278a34a67dd417be2fbe8
Jeferson Veiga <jeferson.veiiga@icloud.com>
2023-05-22 14:08:43 -0300

    Delete M1 directory

--- M1/JavaScript/Praticando com Variaveis/script.js
+++
@@ -1,8 +0,0 @@
-let name = "Jeferson"
-let lastName = "Veiga"
-let birthDate = 1989
-let birthValue = 2022 - birthDate
-let city = "São Paulo"
-
-console.log(`Eu sou o ${name} ${lastName} tenho ${birthValue} anos e moro em ${city}.`)
-
